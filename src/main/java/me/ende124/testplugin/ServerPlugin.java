1b376c80e9027cd1d780e0472ba6d7264e6f40ea
Daniel Liland <danielsen2000@gmail.com>
2015-01-11 15:17:50 +0100

    Added setwarp command.

--- src/main/java/me/ende124/testplugin/ServerPlugin.java
+++ src/main/java/me/ende124/testplugin/ServerPlugin.java
@@ -3,13 +3,21 @@ package me.ende124.testplugin;
 import me.ende124.testplugin.commands.BringCommand;
 import me.ende124.testplugin.commands.GotoCommand;
 import me.ende124.testplugin.commands.HealCommand;
+import me.ende124.testplugin.commands.SetwarpCommand;
+import me.ende124.testplugin.config.MyConfig;
+import me.ende124.testplugin.config.MyConfigManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ServerPlugin extends JavaPlugin {
 
+	public static MyConfig warps;
+	MyConfigManager manager;
+
 	@Override
 	public void onEnable() {
-
+		manager = new MyConfigManager(this);
+		warps = manager.getNewConfig("warps.yml", new String[] {"Warps"});
+		
 		registerCommands();
 	}
 
@@ -21,5 +29,6 @@ public final class ServerPlugin extends JavaPlugin {
 		getCommand("heal").setExecutor(new HealCommand(this));
 		getCommand("goto").setExecutor(new GotoCommand(this));
 		getCommand("bring").setExecutor(new BringCommand(this));
+		getCommand("setwarp").setExecutor(new SetwarpCommand(this));
 	}
 }
