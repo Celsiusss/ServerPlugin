df0a65a7bd56625479df816734b93be640cfc446
Daniel Liland <danielsen2000@gmail.com>
2015-02-01 00:00:18 +0100

    Small fix

--- src/main/java/me/ende124/testplugin/commands/WarpCommand.java
+++ src/main/java/me/ende124/testplugin/commands/WarpCommand.java
@@ -17,16 +17,16 @@ public class WarpCommand extends ServerPluginCommand {
 	
 	@Override
 	public boolean onPlayerCommand(Player p, Command cmd, String label, String[] args) {
-		if (args.length > 0) {
+		if (args.length > 1) {
 			p.sendMessage(getChatColorError() + "Please specify a warp name!");
 			return true;
 		}
-		World w = Bukkit.getServer().getWorld(warps.getString("warps." + args[0] + "world"));
-		double x = warps.getDouble("warps." + args[0] + "x");
-		double y = warps.getDouble("warps." + args[0] + "y");
-		double z = warps.getDouble("warps." + args[0] + "z");
-		float pitch = (float)warps.getDouble("warps." + args[0] + "pitch");
-		float yaw = (float)warps.getDouble("warps." + args[0] + "yaw");
+		World w = Bukkit.getServer().getWorld(warps.getString("warps." + args[0] + ".world"));
+		double x = warps.getDouble("warps." + args[0] + ".x");
+		double y = warps.getDouble("warps." + args[0] + ".y");
+		double z = warps.getDouble("warps." + args[0] + ".z");
+		float pitch = (float)warps.getDouble("warps." + args[0] + ".pitch");
+		float yaw = (float)warps.getDouble("warps." + args[0] + ".yaw");
 		
 		p.teleport(new Location(w, x, y, z, pitch, yaw));
 		return true;
