93ba3a7db393df87e355610b4df0f75b96284c1d
Daniel Liland <danielsen2000@gmail.com>
2015-02-05 20:56:18 +0100

    New commands: kit and acolor

--- src/main/java/me/ende124/testplugin/ServerPlugin.java
+++ src/main/java/me/ende124/testplugin/ServerPlugin.java
@@ -3,9 +3,10 @@ package me.ende124.testplugin;
 import me.ende124.testplugin.commands.*;
 import me.ende124.testplugin.config.MyConfig;
 import me.ende124.testplugin.config.MyConfigManager;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class ServerPlugin extends JavaPlugin {
+public final class ServerPlugin extends JavaPlugin implements Listener {
 
 	public static MyConfig warps;
 	MyConfigManager manager;
@@ -14,14 +15,18 @@ public final class ServerPlugin extends JavaPlugin {
 	public void onEnable() {
 		manager = new MyConfigManager(this);
 		warps = manager.getNewConfig("warps.yml", new String[] {"Warps"});
-		
+
 		registerCommands();
+		
+		getServer().getPluginManager().registerEvents(new AcolorCommand(this), this);
 	}
 
 	@Override
 	public void onDisable() {
 	}
 
+	
+	
 	public void registerCommands() {
 		getCommand("heal").setExecutor(new HealCommand(this));
 		getCommand("goto").setExecutor(new GotoCommand(this));
@@ -30,5 +35,7 @@ public final class ServerPlugin extends JavaPlugin {
 		getCommand("warps").setExecutor(new WarpsCommand(this));
 		getCommand("warp").setExecutor(new WarpCommand(this));
 		getCommand("delwarp").setExecutor(new DelwarpCommand(this));
+		getCommand("kit").setExecutor(new KitCommand(this));
+		getCommand("acolor").setExecutor(new AcolorCommand(this));
 	}
 }
